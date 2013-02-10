b8640bc05cbd5e7c26bfff93791a95d8661b93eb
Jonathan Gould <gouldjb195@potsdam.edu>
2013-02-10 15:41:53 -0500

    Added #5.

---
+++ Euler5.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Euler5
+{
+	public static void main(String[] args)
+	{
+		 int[] list = {20, 19, 18, 17, 16, 14, 13, 11, 7};
+		 boolean stop = false;
+		 int i = 2920;
+		 while (!stop)
+		 {
+			stop = true;
+			for (int j = 0; j < list.length; j++)
+			{
+				if (i % list[j] != 0)
+					stop = false;
+			}
+			if (stop)
+				System.out.println(i);
+			i+=20;
+		 }
+	}
+}
