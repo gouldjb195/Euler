eb8e451913d7c70f47925a5755fb5627f1186caf
Jonathan Gould <gouldjb195@potsdam.edu>
2013-02-10 15:30:43 -0500

    Added #4.

---
+++ Euler4.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Euler4
+{
+	public static void main(String[] args)
+	{
+		int max = 0;
+		for (int i = 0; i < 1000; i++)
+		{
+			for (int j = 0; j < 1000; j++)
+			{
+				if (isPalindrome(i*j) && i*j > max)
+					max = i*j;
+			}
+		}
+		System.out.println(max);
+	}
+
+	private static boolean isPalindrome(int k)
+	{
+		int j = k;
+		int bw = 0;
+		while (k > 1)
+		{
+			bw = bw*10 + k %10;
+			k /= 10;
+		}
+		if (bw == j)
+			return true;
+		return false;
+	}
+
+}
