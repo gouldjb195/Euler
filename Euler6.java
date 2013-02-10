dd3c595a4fa3e9dc80cf22fd979a9d8d4d4abdc8
Jonathan Gould <gouldjb195@potsdam.edu>
2013-02-10 15:51:05 -0500

    Added #6.

---
+++ Euler6.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Euler6
+{
+	public static void main(String[] args)
+	{
+		int sumOfSquares = 0;
+		int sum = 0;
+		for (int i = 1; i <=100; i++)
+		{
+			sumOfSquares += i*i;
+			sum+= i;
+		}
+		System.out.println(sum*sum - sumOfSquares);
+	}
+}
